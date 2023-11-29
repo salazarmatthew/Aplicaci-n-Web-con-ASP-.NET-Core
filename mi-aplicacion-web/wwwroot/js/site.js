c78f269ffdcdf6ee658d54500cfdc6da7a5af2a5
SalazarMatthew <emsalazar1@espe.edu.ec>
2023-11-28 20:23:17 -0500

    2.0

--- mi-aplicacion-web/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // This code will run after the document is loaded
-    console.log('The document is ready!');
-});
-
-function showMessage(message) {
-    // This function shows a message in the console
-    console.log(message);
-}
